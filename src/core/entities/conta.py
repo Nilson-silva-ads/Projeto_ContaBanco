b1a333082a167b8f775d3375b4b7eef593794694
Nilson Silva <nilsonsilva.ads00@gmail.com>
2025-09-13 22:59:31 -0300

    foi acrecentado no projeto o metodo extrato e corrigido codigo do metodo transferir.

--- src/core/entities/conta.py
+++ src/core/entities/conta.py
@@ -12,7 +12,7 @@ class Conta:
             raise print('Saldo insuficiente!!')
         else:
             self.saldo -= valor
-            self.historico.append(f'- {valor} - saque')
+            self.historico.append(f'-{valor} - saque')
             print('Saque realizado com sucesso!')
 
     #Metodo verificar o saldo.
@@ -25,23 +25,38 @@ class Conta:
             raise print('Valor do deposito não pode ser menor que 0 enm menor que 0')
         else:
             self.saldo += valor
-            self.historico.append(f'+ {valor} depósito')
+            self.historico.append(f'+{valor} depósito')
             print('Deposito realizado com sucesso!')
 
+    #Metodo para realizar transferencia entre contas.
     def transferir(self, conta_destino, valor):
-        if valor <= 0:
+        if valor <= 0: #validacao do valor para saber se e menor que zero.
             raise f"Valor da transferencia deve ser maior que zero."
-        if valor > self.saldo:
+        if valor > self.saldo: #Validacao para saber se o saldo nao esta negativo.
             raise print('Saldo insuficiente!!')
         else:
             self.saldo -= valor
-            self.historico.append(f'- {valor} - transferencia')
+            self.historico.append(f'-{valor} -transferencia') #acrescentando no historio a retirada do valor da primeira conta.
             conta_destino.saldo += valor
             print('transferencia realizado com sucesso!')
-            conta_destino.self.append(f'+ {valor} - transferencia')
+            conta_destino.historico.append(f'+{valor} - transferencia')
+
+    def extrato(self):
+        print('Extrato de operaçoes: ')
+        for operacao in self.historico:
+            print(operacao)
+
 
 conta1 = Conta(1001, 'Nilson', 100)
 conta2 = Conta(1002, 'Jessica', 80)
 
 print(f'conta: {conta1.num_conta} cliente: {conta1.nome} Saldo: R${conta1.saldo}')
 print(f'conta: {conta2.num_conta} cliente: {conta2.nome} Saldo: R${conta2.saldo}')
+
+conta1.transferir(conta2, 30)
+conta1.sacar(50)
+
+print(f'conta: {conta1.num_conta} cliente: {conta1.nome} Saldo: R${conta1.saldo}')
+print(f'conta: {conta2.num_conta} cliente: {conta2.nome} Saldo: R${conta2.saldo}')
+
+conta1.extrato()
